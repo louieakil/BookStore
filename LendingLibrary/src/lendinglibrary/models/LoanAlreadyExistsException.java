132a4d16828f13002bd8b89820ae2275871a1f6e
Lewis Scales <lscalesthomas@yahoo.com>
2016-08-03 21:45:04 -0500

    added logic for loan registry

---
+++ LendingLibrary/src/lendinglibrary/models/LoanAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package lendinglibrary.models;
+
+public class LoanAlreadyExistsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ LendingLibrary/src/lendinglibrary/models/LoanNotFoundException.java
@@ -0,0 +1,10 @@
+package lendinglibrary.models;
+
+public class LoanNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
